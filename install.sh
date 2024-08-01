78f71a387a4f730af876be6e644c2c7b56ac54ce
zmlu <16974046+zmlu@users.noreply.github.com>
2024-08-01 17:09:25 +0800

    init

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
-sudo curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
-sudo chmod a+r /etc/apt/keyrings/sagernet.asc
-echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
-  sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y sing-box nginx python3 python3-pip python3-venv
+sudo apt-get install -y nginx python3 python3-pip python3-venv
+bash <(curl -Ls https://github.com/team-cloudchaser/tempest/raw/main/install/singbox.sh)
+bash <(curl -Ls https://github.com/team-cloudchaser/tempest/raw/main/install/xray.sh)
 rm -rf /opt/venv/
 cd /opt && mkdir venv
 cd /opt/venv && python3 -m venv easy-sing-box
