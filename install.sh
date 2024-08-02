fef000a9f7f6871f9bb75a9b9119f4fe2a7b1da4
zmlu <16974046+zmlu@users.noreply.github.com>
2024-08-02 10:01:57 +0800

    init

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 sudo apt-get install -y nginx python3 python3-pip python3-venv
 bash <(curl -fsSL https://github.com/team-cloudchaser/tempest/raw/main/install/singbox.sh)
 bash <(curl -fsSL https://github.com/team-cloudchaser/tempest/raw/main/install/xray.sh)
-bash <(curl -fsSL https://github.com/zmlu/easy-sing-box/raw/main/bbr.sh)
 rm -rf /opt/venv/
 cd /opt && mkdir venv
 cd /opt/venv && python3 -m venv easy-sing-box
@@ -19,3 +18,4 @@ sysctl -w net.core.rmem_max=16777216
 sysctl -w net.core.wmem_max=16777216
 sudo systemctl enable sing-box
 sudo systemctl restart sing-box
+bash <(curl -fsSL https://github.com/zmlu/easy-sing-box/raw/main/bbr.sh)
