fc2c68d80a9e1c4858733baca356e8356f8cd934
zmlu <16974046+zmlu@users.noreply.github.com>
2024-08-02 09:45:10 +0800

    init

--- generate_config.py
+++ generate_config.py
@@ -1,41 +1,12 @@
+import json
 import os
-import random
 import re
 import subprocess
-import uuid
 from jinja2 import Environment, PackageLoader, select_autoescape
+import random
+import uuid
 
-dev_dir = ""
-
-
-def generate_reality_keys():
-    xray_out = subprocess.check_output(['xray', 'x25519'])
-    data_str = xray_out.decode('utf-8')
-    print(f'xray_out: {xray_out}')
-    private_key_pattern = r'Private key: ([\w-]+)'
-    public_key_pattern = r'Public key: ([\w-]+)'
-    private_key_match = re.search(private_key_pattern, data_str)
-    public_key_match = re.search(public_key_pattern, data_str)
-    private_key = private_key_match.group(1) if private_key_match else 'wKpQJg3pYNmHIdYNfcgkOpkuDRjBu_HtT5AILoKIlnA'
-    public_key = public_key_match.group(1) if public_key_match else 'IBFZzGV6xrzrXPCzFMNN3L6paUDNJNoXUbXSKjYEFG4'
-    return private_key, public_key
-
-
-def generate_reality_sid():
-    openssl_out = subprocess.check_output(['openssl', 'rand', '-hex', '4'])
-    data_str = openssl_out.decode('utf-8')
-    data_str = ''.join(data_str.splitlines())
-    return data_str
-
-
-def generate_port():
-    min_value = 9000
-    max_value = 65535
-    random_numbers = random.sample(range(min_value, max_value + 1), 3)
-    h2_port = random_numbers[0]
-    tuic_port = random_numbers[1]
-    reality_port = random_numbers[2]
-    return h2_port, tuic_port, reality_port
+dev_dir = "/Users/zmlu/Developer/github/easy-sing-box/dist"
 
 
 def get_ip():
@@ -49,20 +20,24 @@ def get_ip():
 
 
 if __name__ == '__main__':
-    server_ip = get_ip()
-    private_key, public_key = generate_reality_keys()
-    reality_sid = generate_reality_sid()
-    h2_port, tuic_port, reality_port = generate_port()
-    password = uuid.uuid4()
+    project_dir = os.getcwd()
+    config_file = f'{project_dir}/esb.config'
 
-    # print(f'server_ip: {server_ip}')
-    # print(f'password: {password}')
-    # print(f'h2_port: {h2_port}')
-    # print(f'tuic_port: {tuic_port}')
-    # print(f'reality_port: {reality_port}')
-    # print(f'reality_pbk: {public_key}')
-    # print(f'reality_private_key: {private_key}')
-    # print(f'reality_sid: {reality_sid}')
+    if not os.path.exists(config_file):
+        os.system("python3 generate_esb_config.py")
+
+    with open(config_file, 'r') as file:
+        data = json.load(file)
+
+    server_ip = get_ip()
+    print(f'server_ip: {server_ip}')
+    reality_sid = data.get('reality_sid', 1234)
+    private_key = data.get('private_key', 1234)
+    public_key = data.get('public_key', 1234)
+    password = data.get('password', 1234)
+    h2_port = data.get('h2_port', 1234)
+    tuic_port = data.get('tuic_port', 1234)
+    reality_port = data.get('reality_port', 1234)
 
     env = Environment(
         loader=PackageLoader("generate_config"),
---
+++ generate_esb_config.py
@@ -0,0 +1,70 @@
+import json
+import os
+import random
+import re
+import subprocess
+import uuid
+
+
+def generate_reality_keys():
+    xray_out = subprocess.check_output(['xray', 'x25519'])
+    data_str = xray_out.decode('utf-8')
+    print(f'xray_out: {xray_out}')
+    private_key_pattern = r'Private key: ([\w-]+)'
+    public_key_pattern = r'Public key: ([\w-]+)'
+    private_key_match = re.search(private_key_pattern, data_str)
+    public_key_match = re.search(public_key_pattern, data_str)
+    private_key = private_key_match.group(1) if private_key_match else 'wKpQJg3pYNmHIdYNfcgkOpkuDRjBu_HtT5AILoKIlnA'
+    public_key = public_key_match.group(1) if public_key_match else 'IBFZzGV6xrzrXPCzFMNN3L6paUDNJNoXUbXSKjYEFG4'
+    return private_key, public_key
+
+
+def generate_reality_sid():
+    openssl_out = subprocess.check_output(['openssl', 'rand', '-hex', '4'])
+    data_str = openssl_out.decode('utf-8')
+    data_str = ''.join(data_str.splitlines())
+    return data_str
+
+
+def generate_port():
+    min_value = 9000
+    max_value = 65535
+    random_numbers = random.sample(range(min_value, max_value + 1), 3)
+    h2_port = random_numbers[0]
+    tuic_port = random_numbers[1]
+    reality_port = random_numbers[2]
+    return h2_port, tuic_port, reality_port
+
+
+def generate_esb_config():
+    private_key, public_key = generate_reality_keys()
+    reality_sid = generate_reality_sid()
+    h2_port, tuic_port, reality_port = generate_port()
+    password = str(uuid.uuid4())
+
+    print(f'password: {password}')
+    print(f'h2_port: {h2_port}')
+    print(f'tuic_port: {tuic_port}')
+    print(f'reality_port: {reality_port}')
+    print(f'reality_pbk: {public_key}')
+    print(f'reality_private_key: {private_key}')
+    print(f'reality_sid: {reality_sid}')
+
+    esb_config = {}
+    esb_config['reality_sid'] = reality_sid
+    esb_config['private_key'] = private_key
+    esb_config['public_key'] = public_key
+    esb_config['password'] = password
+    esb_config['h2_port'] = h2_port
+    esb_config['tuic_port'] = tuic_port
+    esb_config['reality_port'] = reality_port
+    project_dir = os.getcwd()
+    config_file = f'{project_dir}/esb.config'
+    if not os.path.exists(project_dir):
+        os.makedirs(project_dir)
+    with open(config_file, 'w') as write_f:
+        write_f.write(json.dumps(esb_config, indent=2, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    generate_esb_config()
