a46f7ac55c96f734b45f4ab6d7c746426c87173e
zmlu <16974046+zmlu@users.noreply.github.com>
2024-11-13 15:55:18 +0800

    fix

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ sudo chmod a+r /etc/apt/keyrings/sagernet.asc
 echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
   sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
 sudo apt-get update
-sudo apt-get install sing-box-beta
+sudo apt-get install sing-box
 echo "重置 venv..."
 rm -rf /opt/venv/
 cd /opt && mkdir venv
