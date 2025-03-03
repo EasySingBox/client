f5af7649112dad1e3137a4688320cea9fae60c1d
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-03 14:29:28 +0800

    update

--- generate_config.py
+++ generate_config.py
@@ -239,7 +239,7 @@ if __name__ == '__main__':
     # os.system(f'echo ""')
 
     if central_api:
-        central_url = f'http://{central_api}/api/hello'
+        central_url = f'http://{central_api}/api/hello?name={www_dir_random_id}'
         response = requests.get(central_url)
         if response.status_code == 200:
             os.system(f'echo ""')
