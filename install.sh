3c708521c0e0168fabc28a1487f989a42dd3f0be
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-04 10:57:13 +0800

    refract to bash

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ sudo curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
 sudo chmod a+r /etc/apt/keyrings/sagernet.asc
 echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
   sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
-sudo apt-get update
-sudo apt-get install sing-box-beta
+sudo apt update
+sudo apt install sing-box-beta
 echo "重置 warp..."
 bash <(curl -fsSL git.io/warp.sh) x
 
