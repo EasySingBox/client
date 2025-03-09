73a5c5190cbff934bd246bd1d92d4742de10d6f5
zmlu <16974046+zmlu@users.noreply.github.com>
2025-03-09 09:41:43 +0800

    Create install-nonf.sh

---
+++ install-nonf.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 检查是否为root下运行
+[[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && exit 1
+
+# 檢查是否提供了第一個參數
+if [ -z "$1" ]; then
+    echo "錯誤：第一個參數 CENTRAL_API 必須填寫！"
+    echo "使用方式: bash <(curl -Ls https://github.com/zmlu/easy-sing-box/raw/main/install.sh) <CENTRAL_API> [RANDOM_PORT_MIN] [RANDOM_PORT_MAX]"
+    exit 1
+fi
+
+CENTRAL_API=${1:-""}
+RANDOM_PORT_MIN=${2:-10000}
+RANDOM_PORT_MAX=${3:-65535}
+
+echo "CENTRAL_API: $CENTRAL_API"
+echo "RANDOM_PORT_MIN: $RANDOM_PORT_MIN"
+echo "RANDOM_PORT_MAX: $RANDOM_PORT_MAX"
+
+apt install -y git
+apt install -y jq
+mkdir /etc/apt/keyrings/ > /dev/null
+# sing-box-beta
+sudo curl -fsSL https://sing-box.app/gpg.key -o /etc/apt/keyrings/sagernet.asc
+sudo chmod a+r /etc/apt/keyrings/sagernet.asc
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/sagernet.asc] https://deb.sagernet.org/ * *" | \
+  sudo tee /etc/apt/sources.list.d/sagernet.list > /dev/null
+sudo apt update
+sudo apt install sing-box-beta
+echo "重置 warp..."
+bash <(curl -fsSL git.io/warp.sh) x
+bash <(curl -Ls https://github.com/zmlu/easy-sing-box/raw/main/update-nonf.sh?_=$(date +%s)) $CENTRAL_API $RANDOM_PORT_MIN $RANDOM_PORT_MAX
