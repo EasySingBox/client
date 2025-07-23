e23bfc65395b33c9bdccce09d699cc4f0056a952
realzmlu <realzmlu@gmail.com>
2025-07-23 14:24:57 +0800

    ss

--- update.sh
+++ update.sh
@@ -28,20 +28,8 @@ function get_ip_info() {
     VPS_ORG=$(echo "$IP_INFO" | jq -r .asOrganization)
 }
 
-function generate_reality_keys() {
-    XRAY_OUT=$(sing-box generate reality-keypair)
-    PRIVATE_KEY=$(echo "$XRAY_OUT" | grep "PrivateKey" | awk '{print $2}')
-    PUBLIC_KEY=$(echo "$XRAY_OUT" | grep "PublicKey" | awk '{print $2}')
-}
-
-function generate_reality_sid() {
-    REALITY_SID=$(sing-box generate rand 4 --hex | tr -d '\n')
-}
-
 function generate_password() {
     SS_PASSWORD=$(sing-box generate rand --base64 32 | tr -d '\n')
-    PASSWORD=$(sing-box generate uuid | tr -d '\n')
-    H2_OBFS_PASSWORD=$(sing-box generate uuid | tr -d '\n')
 }
 
 function generate_port() {
@@ -51,7 +39,7 @@ function generate_port() {
     numbers=()
 
     # 迴圈生成 4 個不重複的隨機數
-    while [ ${#numbers[@]} -lt 5 ]; do
+    while [ ${#numbers[@]} -lt 2 ]; do
         # 生成範圍內的隨機數
         num=$((RANDOM % ($MAX - $MIN + 1) + $MIN))
 
@@ -64,15 +52,10 @@ function generate_port() {
     # 將隨機數賦值給變量
     SS_PORT=${numbers[0]}
     ANYTLS_PORT=${numbers[1]}
-    H2_PORT=${numbers[2]}
-    TUIC_PORT=${numbers[3]}
-    REALITY_PORT=${numbers[4]}
 }
 
 function generate_esb_config() {
     get_ip_info
-    generate_reality_keys
-    generate_reality_sid
     generate_password
     generate_port
 
@@ -81,17 +64,9 @@ function generate_esb_config() {
   "server_ip": "$SERVER_IP",
   "vps_org": "$VPS_ORG",
   "country": "$COUNTRY",
-  "password": "$PASSWORD",
   "ss_password": "$SS_PASSWORD",
   "ss_port": $SS_PORT,
-  "anytls_port": $ANYTLS_PORT,
-  "h2_obfs_password": "$H2_OBFS_PASSWORD",
-  "h2_port": $H2_PORT,
-  "tuic_port": $TUIC_PORT,
-  "reality_port": $REALITY_PORT,
-  "reality_sid": "$REALITY_SID",
-  "public_key": "$PUBLIC_KEY",
-  "private_key": "$PRIVATE_KEY"
+  "anytls_port": $ANYTLS_PORT
 }
 EOF
 }
@@ -105,13 +80,6 @@ function load_esb_config() {
         SS_PASSWORD=$(jq -r .ss_password "$CONFIG_FILE")
         SS_PORT=$(jq -r .ss_port "$CONFIG_FILE")
         ANYTLS_PORT=$(jq -r .anytls_port "$CONFIG_FILE")
-        H2_OBFS_PASSWORD=$(jq -r .h2_obfs_password "$CONFIG_FILE")
-        H2_PORT=$(jq -r .h2_port "$CONFIG_FILE")
-        TUIC_PORT=$(jq -r .tuic_port "$CONFIG_FILE")
-        REALITY_PORT=$(jq -r .reality_port "$CONFIG_FILE")
-        REALITY_SID=$(jq -r .reality_sid "$CONFIG_FILE")
-        PUBLIC_KEY=$(jq -r .public_key "$CONFIG_FILE")
-        PRIVATE_KEY=$(jq -r .private_key "$CONFIG_FILE")
     else
         generate_esb_config
         load_esb_config
@@ -179,81 +147,6 @@ function generate_singbox_server() {
         "alpn": [
           "h2",
           "http/1.1"
-        ],
-        "certificate_path": "/etc/sing-box/cert.pem",
-        "key_path": "/etc/sing-box/private.key"
-      }
-    },
-    {
-      "type": "hysteria2",
-      "tag": "hy2",
-      "listen": "::",
-      "listen_port": $H2_PORT,
-      "up_mbps": 500,
-      "down_mbps": 500,
-      "users": [
-        {
-          "name": "user-jacob",
-          "password": "$PASSWORD"
-        }
-      ],
-      "tls": {
-        "enabled": true,
-        "alpn": "h3",
-        "certificate_path": "$SING_BOX_CONFIG_DIR/cert.pem",
-        "key_path": "$SING_BOX_CONFIG_DIR/private.key"
-      },
-      "obfs": {
-        "type": "salamander",
-        "password": "$H2_OBFS_PASSWORD"
-      },
-      "masquerade": {
-        "type": "string",
-        "status_code": 500,
-        "content": "The server was unable to complete your request. Please try again later. If this problem persists, please contact support. Server logs contain details of this error with request ID: 839-234."
-      }
-    },
-    {
-      "type": "vless",
-      "tag": "vless",
-      "listen": "::",
-      "listen_port": $REALITY_PORT,
-      "users": [
-        {
-          "uuid": "$PASSWORD",
-          "flow": "xtls-rprx-vision"
-        }
-      ],
-      "tls": {
-        "enabled": true,
-        "server_name": "yahoo.com",
-        "reality": {
-          "enabled": true,
-          "handshake": {
-            "server": "yahoo.com",
-            "server_port": 443
-          },
-          "private_key": "$PRIVATE_KEY",
-          "short_id": "$REALITY_SID"
-        }
-      }
-    },
-    {
-      "type": "tuic",
-      "tag": "tuic5",
-      "listen": "::",
-      "listen_port": $TUIC_PORT,
-      "users": [
-        {
-          "uuid": "$PASSWORD",
-          "password": "$PASSWORD"
-        }
-      ],
-      "congestion_control": "bbr",
-      "tls": {
-        "enabled": true,
-        "alpn": [
-          "h3"
         ],
         "certificate_path": "/etc/sing-box/cert.pem",
         "key_path": "/etc/sing-box/private.key"
