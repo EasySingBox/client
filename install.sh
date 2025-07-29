0bd250f761c3a3ad6c97eae17258ad87ca66391a
realzmlu <realzmlu@gmail.com>
2025-07-29 10:23:27 +0800

    sb update2

--- install.sh
+++ install.sh
@@ -3,13 +3,6 @@
 # 检查是否为root下运行
 [[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && exit 1
 
-# 檢查是否提供了第一個參數
-if [ -z "$1" ]; then
-    echo "錯誤：第一個參數 CENTRAL_API 必須填寫！"
-    echo "使用方式: bash <(curl -Ls https://codeberg.org/easy-sing-box/client/raw/main/install.sh) <CENTRAL_API> [RANDOM_PORT_MIN] [RANDOM_PORT_MAX]"
-    exit 1
-fi
-
 CENTRAL_API=${1:-""}
 RANDOM_PORT_MIN=${2:-10000}
 RANDOM_PORT_MAX=${3:-65535}
