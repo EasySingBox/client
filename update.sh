ec51d39a58a7ff1b222c0fae973ee3ef21ccb7ac
realzmlu <realzmlu@gmail.com>
2025-08-07 13:23:02 +0800

    sb update2

--- update.sh
+++ update.sh
@@ -3,6 +3,13 @@
 # 检查是否为root下运行
 [[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && exit 1
 
+# 檢查是否提供了第一個參數
+if [ -z "$1" ]; then
+    echo "錯誤：第一個參數 CENTRAL_API 必須填寫！"
+    echo "使用方式: bash <(curl -Ls https://codeberg.org/easy-sing-box/client/raw/main/update.sh) <CENTRAL_API> [RANDOM_PORT_MIN] [RANDOM_PORT_MAX]"
+    exit 1
+fi
+
 echo "开始生成配置..."
 
 CONFIG_FILE="$HOME/esb.config"
@@ -12,9 +19,7 @@ CENTRAL_API="$1"
 MIN=${2:-10000}
 MAX=${3:-65535}
 
-if [ -z "$1" ]; then
 echo "CENTRAL_API: $CENTRAL_API"
-fi
 echo "RANDOM_PORT_MIN: $MIN"
 echo "RANDOM_PORT_MAX: $MAX"
 
@@ -253,9 +258,7 @@ systemctl restart snell
 clear
 echo -e "\e[1;33mSuccess!\033[0m"
 
-if [ -z "$CENTRAL_API" ]; then
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$CENTRAL_API/api/hello" -H "Content-Type: application/json" --data @$CONFIG_FILE)
-  if [[ "$RESPONSE_CODE" == "200" ]]; then
-      echo "推送到 Central API 成功 ($CENTRAL_API)"
-  fi
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$CENTRAL_API/api/hello" -H "Content-Type: application/json" --data @$CONFIG_FILE)
+if [[ "$RESPONSE_CODE" == "200" ]]; then
+    echo "推送到 Central API 成功 ($CENTRAL_API)"
 fi
