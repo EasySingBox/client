6202b09c855b0af4afe3954d6edda2f76c504f09
realzmlu <realzmlu@gmail.com>
2025-12-08 15:14:24 +0800

    update package_names

--- install.sh
+++ install.sh
@@ -3,36 +3,25 @@
 # 检查是否为root下运行
 [[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && exit 1
 
-ARGO_DOMAIN=${1:-''}
-ARGO_AUTH=${2:-''}
+CENTRAL_API=${1:-""}
+RANDOM_PORT_MIN=${2:-10000}
+RANDOM_PORT_MAX=${3:-65535}
 
-apt install -y jq
+echo "CENTRAL_API: $CENTRAL_API"
+echo "RANDOM_PORT_MIN: $RANDOM_PORT_MIN"
+echo "RANDOM_PORT_MAX: $RANDOM_PORT_MAX"
 
-IP_INFO=$(curl -4 https://free.freeipapi.com/api/json)
-SERVER_IP=$(echo "$IP_INFO" | jq -r .ipAddress)
-COUNTRY=$(echo "$IP_INFO" | jq -r .countryCode)
-VPS_ORG_FULL=$(echo "$IP_INFO" | jq -r .asnOrganization)
-VPS_ORG=$(echo "$VPS_ORG_FULL" | cut -d' ' -f1)
-PASSWORD=$(cat /proc/sys/kernel/random/uuid)
+apt install -y git jq gcc wget unzip curl
+mkdir /etc/apt/keyrings/ > /dev/null
 
-echo "SERVER_IP: $SERVER_IP"
-echo "PASSWORD: $PASSWORD"
-echo "ARGO_DOMAIN: $ARGO_DOMAIN"
-echo "ARGO_AUTH: $ARGO_AUTH"
-echo "[$COUNTRY] $VPS_ORG"
+sudo apt remove -y sing-box
+sudo apt remove -y sing-box-beta
+
+# install sing-box-beta
+curl -fsSL https://sing-box.app/install.sh | sh -s -- --beta
+
+# install hy2-tcp
+bash <(curl -fsSL https://tcp.hy2.sh/)
 
 # gen config
-bash <(wget -qO- https://raw.githubusercontent.com/zmlu/sing-box/main/sing-box.sh) \
-  --LANGUAGE c \
-  --CHOOSE_PROTOCOLS a \
-  --START_PORT 10000 \
-  --PORT_NGINX 60000 \
-  --SERVER_IP $SERVER_IP \
-  --CDN skk.moe \
-  --UUID_CONFIRM $PASSWORD \
-  --SUBSCRIBE=true \
-  --ARGO=true \
-  --ARGO_DOMAIN=$ARGO_DOMAIN \
-  --ARGO_AUTH="sudo cloudflared service install $ARGO_AUTH" \
-  --PORT_HOPPING_RANGE 50000:51000 \
-  --NODE_NAME_CONFIRM="[$COUNTRY] $VPS_ORG"
+bash <(curl -Ls https://raw.githubusercontent.com/EasySingBox/client/refs/heads/main/update.sh?_=$(date +%s)) $CENTRAL_API $RANDOM_PORT_MIN $RANDOM_PORT_MAX
