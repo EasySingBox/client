0efd456ae3ab178cf3871d2cd8d2b66594ae3916
realzmlu <realzmlu@gmail.com>
2026-01-04 16:15:05 +0800

    update sing-box config

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 # 检查是否为root下运行
 [[ $EUID -ne 0 ]] && echo -e '\033[1;35m请在root用户下运行脚本\033[0m' && exit 1
 
-RANDOM_PORT_MIN=${1:-10000}
-RANDOM_PORT_MAX=${2:-65535}
+echo "开始生成配置..."
+
+CONFIG_FILE="$HOME/esb.config"
+SING_BOX_CONFIG_DIR="/etc/sing-box"
+
+MIN=${1:-10000}
+MAX=${2:-65535}
 
 echo "RANDOM_PORT_MIN: $RANDOM_PORT_MIN"
 echo "RANDOM_PORT_MAX: $RANDOM_PORT_MAX"
@@ -17,9 +22,292 @@ sudo apt remove -y sing-box
 # install sing-box-beta
 curl -fsSL https://sing-box.app/install.sh | sh -s -- --beta
 
+
+function get_ip_info() {
+    IP_INFO=$(curl -4 https://ip.cloudflare.nyc.mn)
+    SERVER_IP=$(echo "$IP_INFO" | jq -r .ip)
+}
+
+function generate_reality_keys() {
+    XRAY_OUT=$(sing-box generate reality-keypair)
+    PRIVATE_KEY=$(echo "$XRAY_OUT" | grep "PrivateKey" | awk '{print $2}')
+    PUBLIC_KEY=$(echo "$XRAY_OUT" | grep "PublicKey" | awk '{print $2}')
+}
+
+function generate_reality_sid() {
+    REALITY_SID=$(sing-box generate rand 4 --hex | tr -d '\n')
+}
+
+function generate_password() {
+    SS_PASSWORD=$(sing-box generate rand --base64 32 | tr -d '\n')
+    PASSWORD=$(sing-box generate uuid | tr -d '\n')
+    H2_OBFS_PASSWORD=$(sing-box generate uuid | tr -d '\n')
+}
+
+function generate_port() {
+    numbers=()
+    while [ ${#numbers[@]} -lt 4 ]; do
+        num=$((RANDOM % ($MAX - $MIN + 1) + $MIN))
+        if [[ ! " ${numbers[@]} " =~ " $num " ]]; then
+            numbers+=($num)
+        fi
+    done
+
+    H2_PORT=${numbers[0]}
+    TUIC_PORT=${numbers[1]}
+    SS_PORT=${numbers[2]}
+    REALITY_PORT=${numbers[3]}
+}
+
+function generate_esb_config() {
+    get_ip_info
+    generate_reality_keys
+    generate_reality_sid
+    generate_password
+    generate_port
+
+    cat <<EOF > "$CONFIG_FILE"
+{
+  "server_ip": "$SERVER_IP",
+  "password": "$PASSWORD",
+  "ss_password": "$SS_PASSWORD",
+  "ss_port": $SS_PORT,
+  "h2_obfs_password": "$H2_OBFS_PASSWORD",
+  "h2_port": $H2_PORT,
+  "tuic_port": $TUIC_PORT,
+  "reality_port": $REALITY_PORT,
+  "reality_sid": "$REALITY_SID",
+  "public_key": "$PUBLIC_KEY",
+  "private_key": "$PRIVATE_KEY"
+}
+EOF
+}
+
+function load_esb_config() {
+    if [[ -f "$CONFIG_FILE" ]]; then
+        SERVER_IP=$(jq -r .server_ip "$CONFIG_FILE")
+        PASSWORD=$(jq -r .password "$CONFIG_FILE")
+        SS_PASSWORD=$(jq -r .ss_password "$CONFIG_FILE")
+        SS_PORT=$(jq -r .ss_port "$CONFIG_FILE")
+        H2_OBFS_PASSWORD=$(jq -r .h2_obfs_password "$CONFIG_FILE")
+        H2_PORT=$(jq -r .h2_port "$CONFIG_FILE")
+        TUIC_PORT=$(jq -r .tuic_port "$CONFIG_FILE")
+        REALITY_PORT=$(jq -r .reality_port "$CONFIG_FILE")
+        REALITY_SID=$(jq -r .reality_sid "$CONFIG_FILE")
+        PUBLIC_KEY=$(jq -r .public_key "$CONFIG_FILE")
+        PRIVATE_KEY=$(jq -r .private_key "$CONFIG_FILE")
+    else
+        generate_esb_config
+        load_esb_config
+    fi
+}
+
+function generate_singbox_server() {
+    load_esb_config
+
+    rm -rf $SING_BOX_CONFIG_DIR
+    mkdir -p "$SING_BOX_CONFIG_DIR"
+
+    wget --inet4-only -O "$SING_BOX_CONFIG_DIR/cert.pem" https://raw.githubusercontent.com/EasySingBox/client/refs/heads/main/cert/cert.pem
+    wget --inet4-only -O "$SING_BOX_CONFIG_DIR/private.key" https://raw.githubusercontent.com/EasySingBox/client/refs/heads/main/cert/private.key
+    wget --inet4-only -O "$SING_BOX_CONFIG_DIR/netflix.srs" https://cdn.jsdelivr.net/gh/DustinWin/ruleset_geodata@sing-box-ruleset/netflix.srs
+    wget --inet4-only -O "$SING_BOX_CONFIG_DIR/netflixip.srs" https://cdn.jsdelivr.net/gh/DustinWin/ruleset_geodata@sing-box-ruleset/netflixip.srs
+
+    cat <<EOF > "$SING_BOX_CONFIG_DIR/config.json"
+{
+  "log": {
+    "level": "error",
+    "timestamp": true
+  },
+  "dns": {
+    "servers": [
+      {
+        "type": "local",
+        "tag": "dns"
+      }
+    ],
+    "independent_cache": true
+  },
+  "inbounds": [
+    {
+      "type": "shadowsocks",
+      "tag": "ss",
+      "listen": "::",
+      "listen_port": $SS_PORT,
+      "method": "2022-blake3-aes-256-gcm",
+      "password": "$SS_PASSWORD"
+    },
+    {
+      "type": "tuic",
+      "tag": "tuic5",
+      "listen": "::",
+      "listen_port": $TUIC_PORT,
+      "sniff": true,
+      "sniff_override_destination": true,
+      "users": [
+        {
+          "uuid": "$PASSWORD",
+          "password": "$PASSWORD"
+        }
+      ],
+      "congestion_control": "bbr",
+      "tls": {
+        "enabled": true,
+        "alpn": "h3",
+        "certificate_path": "/etc/sing-box/cert.pem",
+        "key_path": "/etc/sing-box/private.key"
+      }
+    },
+    {
+      "type": "vless",
+      "tag": "vless",
+      "listen": "::",
+      "listen_port": $REALITY_PORT,
+      "users": [
+        {
+          "uuid": "$PASSWORD",
+          "flow": "xtls-rprx-vision"
+        }
+      ],
+      "tls": {
+        "enabled": true,
+        "alpn": "h3",
+        "reality": {
+          "enabled": true,
+          "handshake": {
+            "server": "yahoo.com",
+            "server_port": 443
+          },
+          "private_key": "$PRIVATE_KEY",
+          "short_id": "$REALITY_SID"
+        }
+      }
+    },
+    {
+      "type": "hysteria2",
+      "tag": "hy2",
+      "listen": "::",
+      "listen_port": $H2_PORT,
+      "sniff": true,
+      "sniff_override_destination": true,
+      "users": [
+        {
+          "name": "user-jacob",
+          "password": "$PASSWORD"
+        }
+      ],
+      "tls": {
+        "enabled": true,
+        "alpn": "h3",
+        "certificate_path": "$SING_BOX_CONFIG_DIR/cert.pem",
+        "key_path": "$SING_BOX_CONFIG_DIR/private.key"
+      },
+      "masquerade": {
+        "type": "string",
+        "status_code": 500,
+        "content": "The server was unable to complete your request. Please try again later. If this problem persists, please contact support. Server logs contain details of this error with request ID: 839-234."
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "type": "direct",
+      "tag": "direct",
+      "domain_resolver": {
+        "server": "dns",
+        "strategy": "prefer_ipv4"
+      }
+    },
+    {
+      "type": "direct",
+      "tag": "wgcf",
+      "routing_mark": 51888,
+      "domain_resolver": {
+        "server": "dns",
+        "strategy": "ipv6_only"
+      }
+    }
+  ],
+  "route": {
+    "rules": [
+      {
+        "action": "sniff"
+      },
+      {
+        "protocol": "dns",
+        "action": "hijack-dns"
+      },
+      {
+        "protocol": [
+          "stun"
+        ],
+        "outbound": "direct"
+      },
+      {
+        "rule_set": [
+          "netflix",
+          "netflixip"
+        ],
+        "outbound": "wgcf"
+      }
+    ],
+    "rule_set": [
+      {
+        "type": "local",
+        "tag": "netflix",
+        "format": "binary",
+        "path": "$SING_BOX_CONFIG_DIR/netflix.srs"
+      },
+      {
+        "type": "local",
+        "tag": "netflixip",
+        "format": "binary",
+        "path": "$SING_BOX_CONFIG_DIR/netflixip.srs"
+      }
+    ],
+    "final": "direct",
+    "default_domain_resolver": "dns"
+  }
+}
+EOF
+}
+
+if [[ ! -f "$CONFIG_FILE" ]]; then
+    generate_esb_config
+fi
+
+load_esb_config
+
+generate_singbox_server
+
+cat <<EOF > "/usr/lib/systemd/system/sing-box.service"
+[Unit]
+Description=sing-box service
+Documentation=https://sing-box.sagernet.org
+After=network.target nss-lookup.target network-online.target
+
+[Service]
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_PTRACE CAP_DAC_READ_SEARCH
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_RAW CAP_NET_BIND_SERVICE CAP_SYS_PTRACE CAP_DAC_READ_SEARCH
+ExecStart=/usr/bin/sing-box -D /var/lib/sing-box -C /etc/sing-box run
+ExecReload=/bin/kill -HUP $MAINPID
+Restart=on-failure
+RestartSec=10s
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "重启 sing-box..."
+systemctl daemon-reload
+systemctl restart sing-box
+systemctl enable sing-box
+
+echo "重启 warp..."
 bash <(curl -fsSL http://git.io/warp.sh) x
-
-# gen config
-bash <(curl -Ls https://raw.githubusercontent.com/EasySingBox/client/main/update.sh?_=$(date +%s)) $RANDOM_PORT_MIN $RANDOM_PORT_MAX
-
 bash <(curl -fsSL http://git.io/warp.sh) rwg
+
+echo -e "\e[1;33mSuccess!\033[0m"
+
+cat $HOME/esb.config
