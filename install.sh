d8f9bf4e5baf4ec346bf8daaebab7cfb416b4f6f
realzmlu <realzmlu@gmail.com>
2026-02-28 11:15:37 +0800

    v2

--- install.sh
+++ install.sh
@@ -268,7 +268,6 @@ function generate_singbox_server() {
           "password": "$PASSWORD"
         }
       ],
-      "quic_congestion_control": "bbr2",
       "tls": {
         "enabled": true,
         "server_name": "$DOMAIN_NAME",
