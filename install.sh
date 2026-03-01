35a547fb89feb82a67c399c03835d78730d26a6e
realzmlu <realzmlu@gmail.com>
2026-03-01 10:19:43 +0800

    Revert "v2"
    This reverts commit 6ebc177de129d4a50d279b7271a411cdde47dc31.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ DOMAIN_NAME="$2"
 MIN=10000
 MAX=65535
 
-apt install -y git jq gcc wget unzip curl socat cron nginx
+apt install -y git jq gcc wget unzip curl socat cron
 mkdir /etc/apt/keyrings/ > /dev/null
 
 sudo apt remove -y sing-box
