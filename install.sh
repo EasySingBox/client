1f559ce8570b5a5176a132438db2c803d4f108bf
realzmlu <realzmlu@gmail.com>
2026-03-06 14:35:35 +0800

    v12

--- install.sh
+++ install.sh
@@ -316,31 +316,6 @@ function generate_singbox_server() {
           "short_id": "$REALITY_SID"
         }
       }
-    },
-    {
-      "type": "naive",
-      "tag": "naive-in",
-      "listen": "::",
-      "listen_port": 443,
-      "tcp_fast_open": true,
-      "tcp_multi_path": true,
-      "quic_congestion_control": "bbr2",
-      "users": [
-        {
-          "username": "user-zmlu",
-          "password": "$PASSWORD"
-        }
-      ],
-      "tls": {
-        "enabled": true,
-        "server_name": "$DOMAIN_NAME",
-        "certificate_path": "$CERT_DIR/fullchain.pem",
-        "key_path": "$CERT_DIR/privkey.pem",
-        "ech": {
-          "enabled": false,
-          "key": $ECH_KEYS
-        }
-      },
     }
   ],
   "outbounds": [
